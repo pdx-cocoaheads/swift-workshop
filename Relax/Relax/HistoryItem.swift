26bcc61da1bd253de2a2da9fb9888b605e8da59c
Ryan Arana <ryan@ryanarana.com>
2015-02-09 14:18:13 -0800

    Add TableView to HistoryViewController

---
+++ Relax/Relax/HistoryItem.swift
@@ -0,0 +1,15 @@
+//
+//  HistoryItem.swift
+//  Relax
+//
+//  Created by Ryan Arana on 2/9/15.
+//  Copyright (c) 2015 PDX-iOS. All rights reserved.
+//
+
+import Foundation
+
+struct HistoryItem {
+    let activity: Activity
+    let rating: Double
+    let comments: String
+}
---
+++ Relax/Relax/HistoryTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  HistoryTableViewCell.swift
+//  Relax
+//
+//  Created by Ryan Arana on 2/9/15.
+//  Copyright (c) 2015 PDX-iOS. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func configureWithHistoryItem(item: HistoryItem) {
+        // TODO: Implement me!
+    }
+
+}
